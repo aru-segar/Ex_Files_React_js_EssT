caa675ce7348a1174d65c61e1bf2e76d316a9070
aru_segar <arunigagnanasegaran@gmail.com>
2025-06-09 17:37:55 +0530

    Destructuring arrays and objects

--- Exercise Files/Ch04/04_03/start/react-app/src/App.js
+++ Exercise Files/Ch04/04_03/start/react-app/src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
 
-function App() {
+const [firstCity, secondCity] = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
+
+console.log(firstCity);
+console.log(secondCity);
+
+function App({library}) {
   return (
     <div className="App">
-      <h1>Hello from React</h1>
+      <h1>Hello from {library}</h1>
     </div>
   );
 }
